aea000f807d8f4f310f8c72ad20ad2da07c3c35a
dangvanquangdtu <dangvanquangdtu@gmail.com>
2021-03-24 15:24:50 +0700

    Update Project first time

---
+++ DoAnTotNghiep_BanDienThoai/DoAnTotNghiep_BanDienThoai/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAnTotNghiep_BanDienThoai.MvcApplication" Language="C#" %>
---
+++ DoAnTotNghiep_BanDienThoai/DoAnTotNghiep_BanDienThoai/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopConnection;
+
+namespace DoAnTotNghiep_BanDienThoai.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new ShopConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where TinhTrang = 0");
+        }
+    }
+}
